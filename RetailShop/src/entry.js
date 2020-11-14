2570a811af579aec6849e98b8d0f0a33ebc9fab1
Michael <mike787b@gmail.com>
2020-11-14 14:18:00 +1300

    Installed webpack through npm package manager and setup msbuild to run webpack bundling on build. Installed bootstrap and add dummy html elements for testing purposes.

---
+++ RetailShop/src/entry.js
@@ -0,0 +1,12 @@
+﻿// import dependencies for webpack
+// JS
+import 'bootstrap';
+import 'jquery';
+import 'jquery-validation';
+import 'jquery-validation-unobtrusive';
+import './js/site.js';
+
+// CSS
+//require('./../node_modules/bootstrap/dist/css/bootstrap.css');
+//require('./css/site.css');
+require('./scss/styles.scss');
